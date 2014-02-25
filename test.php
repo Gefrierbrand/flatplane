9e4ec119bf7a6126fb8869859234301caf74d983
Gefrierbrand <nineff@gmail.com>
2014-02-25 20:46:12 +0100

    First working formula example

--- test.php
+++ test.php
@@ -1,68 +1,51 @@
 <?php
-require_once('tcpdf-code/tcpdf.php');
-date_default_timezone_set('Europe/Berlin');
+
+	require_once ('tcpdf-code/tcpdf.php');
+	date_default_timezone_set('Europe/Berlin');
+
+	// create new PDF document
+	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+	// set document information
+	$pdf->SetCreator(PDF_CREATOR);
+	$pdf->SetAuthor('Nikolai Neff');
+	$pdf->SetTitle('Titel des Dokuments');
+	$pdf->SetSubject('Thema');
+	$pdf->SetKeywords('KEYWORD1, keyword2');
 
 
-// create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+	$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nikolai Neff');
-$pdf->SetTitle('Titel des Dokuments');
-$pdf->SetSubject('SUBJEKT');
-$pdf->SetKeywords('KEYWORD1, keyword2');
+	// set margins
+	$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+	$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+	$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
+	// set auto page breaks
+	$pdf->SetAutoPageBreak(false, PDF_MARGIN_BOTTOM);
 
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+	// set image scale factor
+	$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-// set some language-dependent strings (optional)
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-    require_once(dirname(__FILE__).'/lang/eng.php');
-    $pdf->setLanguageArray($l);
-}
+	// set some language-dependent strings (optional)
+	if (@file_exists(dirname(__file__) . '/lang/eng.php'))
+	{
+		require_once (dirname(__file__) . '/lang/eng.php');
+		$pdf->setLanguageArray($l);
+	}
 
 
-// set default font subsetting mode
-$pdf->setFontSubsetting(true);
+	// set default font subsetting mode
+	$pdf->setFontSubsetting(true);
 
-// Set font
-// dejavusans is a UTF-8 Unicode font, if you only need to
-// print standard ASCII chars, you can use core fonts like
-// helvetica or times to reduce file size.
-$pdf->SetFont('dejavusans', '', 14, '', true);
+	$pdf->AddPage();
 
-// Add a page
-// This method has several options, check the source code documentation for more information.
-$pdf->AddPage();
+    $pdf->setRasterizeVectorImages(false);
+    //$pdf->Write(1,'test');
 
-// Set some content to print
-$html = <<<EOD
-<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-<i>This is the first example of TCPDF library.</i>
-<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-<p>Please check the source code documentation and other examples for further information.</p>
-<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-EOD;
+    $pdf->ImageSVG('test.svg', $x=5, $y=100, $w=200, $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+	// ---------------------------------------------------------
 
-// Print text using writeHTMLCell()
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
-
-// ---------------------------------------------------------
-
-// Close and output PDF document
-// This method has several options, check the source code documentation for more information.
-$pdf->Output('example_001.pdf', 'F');
+	$pdf->Output('output/example_001.pdf', 'F');
 
 ?>
