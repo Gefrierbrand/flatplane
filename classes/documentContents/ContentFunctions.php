29b88103ddacee2ba4b4d971e7f6fd704f03b092
Gefrierbrand <nineff@gmail.com>
2014-04-17 19:27:06 +0200

    documentation update

--- classes/documentContents/ContentFunctions.php
+++ classes/documentContents/ContentFunctions.php
@@ -23,18 +23,25 @@ namespace de\flatplane\documentContents;
 
 use de\flatplane\documentContent\DocumentContent;
 use de\flatplane\interfaces\DocumentContentElementInterface;
+use de\flatplane\structure\Document;
 use de\flatplane\utilities\Counter;
 use de\flatplane\utilities\Number;
 
-//TODO: FIX ME
-
 /**
- * Description of Content
+ * This trait provides functionality to the Document and DocumentContentElement
+ * classes for dealing with content elements like adding and numbering items.
+ * @internal
+ *  This might get extended to be able to move or delete content elements,
+ *  which is currently not needed.
  *
  * @author Nikolai Neff <admin@flatplane.de>
  */
 trait ContentFunctions
 {
+    /**
+     * @var array
+     *  Array containing instances of the number object representing a counted value
+     */
     protected $numbers = array();
     protected $numberingLevel = -1;
     protected $numberingStyle = 'int';
@@ -45,19 +52,31 @@ trait ContentFunctions
      *  text or formulas
      */
     protected $content;
+
+    /**
+     * @var array
+     *  Named-key array holding instances of Counter
+     */
     protected $counter = array();
 
     /**
      * @var mixed
      *  Set to true or false to completely allow or dissallow subcontent
-     *  Set to an array containing the type names to allow specific subcontent types
+     *  Set to an array containing the type names to allow specific content types
      */
-    protected $allowSubContent = true; // use specific types in an array or forbid completely by using false
+    protected $allowSubContent = true;
 
+    /**
+     * @var int
+     *  integer representing the depth of the current object inside the Document
+     *  tree, starting at 0 for the root (Document).
+     */
     protected $level = 0;
 
     /**
-     * TODO: update inline doc
+     * This method is used to add content to the Document or other content.
+     * It checks if the given, to-be added content-type is allowed for the current
+     * object and returns false on failure or a reference to the added content.
      * @param DocumentContentElementInterface $content
      * @return DocumentContent|bool
      */
@@ -72,7 +91,11 @@ trait ContentFunctions
             return false;
         }
 
+        //set the contents parent to the current instance to be able to reversely
+        //traverse the document tree
         $content->setParent($this);
+
+        //the subcontents level is always one greater than the current level
         $content->setLevel($this->getLevel()+1);
 
         //the number property is only set if the enumerate property is true
@@ -80,12 +103,14 @@ trait ContentFunctions
             $this->calculateNumber($content);
         }
 
-        //each content needs to know its parent to be able to reversely traverse
-        //the document tree up to its root.
-
         return $this->content[] = $content;
     }
 
+    /**
+     * Determines if the given content may be added to the current object
+     * @param DocumentContentElementInterface $content
+     * @return bool
+     */
     protected function checkAllowedContent(DocumentContentElementInterface $content)
     {
         if ($this->allowSubContent === true) {
@@ -96,16 +121,38 @@ trait ContentFunctions
             return false;
         }
     }
+
     /**
-     * TODO: DOCUMENT INLINE
-     * @param \de\flatplane\interfaces\DocumentContentElementInterface $content
+     * This method delegates the calculation of the content-number to either its
+     * own instance if the numberingLevel permits it, or to another object higher
+     * up in the document tree. It then sets the number in the subcontent.
+     * @param DocumentContentElementInterface $content
+     * @throws OutOfBoundsException
      */
     protected function calculateNumber(DocumentContentElementInterface $content)
     {
+        //check the contents numberingLevel settings (-1 for arbitrary depth)
         if ($content->getNumberingLevel() == -1) {
+            //increment the appropriate counters in the current depth and
+            //get their value
             $counterValue = $this->checkLocalCounter($content)->getValue();
-        } else {
+            //the parent part of the childs number is the current number
+            $parentnum = $this->getNumbers();
+        } else if ($content->getNumberingLevel() >= 0) {
+            //increment the appropriate counters in the parent at the correct
+            //depth and get their value
             $counterValue = $this->checkRemoteCounter($content)->getValue();
+
+            //remove unneccesary parts from the current numbering scheme
+            $parentnum = array_slice(
+                $this->getNumbers(),
+                0,
+                $content->getNumberingLevel()
+            );
+        } else {
+            throw new \OutOfBoundsException(
+                'The numberingLevel can\'t be smaller than -1'
+            );
         }
 
         //set the Number as an instance of the Number object to have access
@@ -113,24 +160,13 @@ trait ContentFunctions
         $num = new Number($counterValue);
         $num->setFormat($this->numberingStyle);
 
-        //todo: doc, FIX?
-        if ($content->getNumberingLevel() != -1) {
-            $parentnum = array_slice(
-                $this->getNumbers(),
-                0,
-                $content->getNumberingLevel()
-            );
-        } else {
-            $parentnum = $this->getNumbers();
-        }
-
+        //append the new content number to the calculated parents
         array_push($parentnum, $num);
-
         $content->setNumbers($parentnum);
     }
 
     /**
-     * checks if a counter for the content-type already exists and increments
+     * Checks if a counter for the content-type already exists and increments
      * its value, or creates a new one for that type
      * @param DocumentContentElementInterface $content
      * @return Counter Counter for the given content-type
@@ -152,6 +188,12 @@ trait ContentFunctions
         return $this->counter[$content->getType()];
     }
 
+    /**
+     * Calls the checkLocalCounter() method at the appropriate level in the
+     * document tree
+     * @param DocumentContentElementInterface $content
+     * @return Counter
+     */
     protected function checkRemoteCounter(DocumentContentElementInterface $content)
     {
         $level = $content->getNumberingLevel();
@@ -163,10 +205,11 @@ trait ContentFunctions
         }
     }
 
-     /**
-     * @return array
-     *  Returns a (multilevel) Array containing references to PageElement instances
-     */
+    /**
+    * @return array
+    *  Returns a (multilevel) Array containing references to
+    *  DocumentContentElement instances
+    */
     public function getContent()
     {
         return $this->content;
@@ -175,23 +218,28 @@ trait ContentFunctions
     /**
      * @see getContent() :Alias:
      * @return array
-     *  Returns empty array or array containing instances of Document
      */
     public function getChildren() // Alias of getSections()
     {
         return $this->getContent();
     }
 
+    /**
+     * @return bool
+     */
     public function hasContent()
     {
         return !empty($this->content);
     }
 
     /**
-    *
-    * @param string $name
-    * @return Counter
-    */
+     * Returns an existing Counter for the given type or creates a new one if
+     * a counter for that type is not already present. This might create
+     * unwanted side effects like wrong element-numbering and therefore also
+     * triggers an error in that case.
+     * @param string $name
+     * @return Counter
+     */
     public function getCounter($name)
     {
         if (array_key_exists($name, $this->counter)) {
@@ -203,6 +251,12 @@ trait ContentFunctions
         }
     }
 
+    /**
+     * Adds a new Counter for the given type to the current object
+     * @param Counter $counter
+     * @param string $name
+     * @return Counter
+     */
     protected function addCounter(Counter $counter, $name)
     {
         return $this->counter[$name] = $counter;
@@ -223,8 +277,8 @@ trait ContentFunctions
     }
 
     /**
-     *
-     * @return Document|DocumentContentElement
+     * This method calls itself recursively until the root Document is reached
+     * @return DocumentContentStructure
      */
     public function toRoot()
     {
@@ -236,9 +290,11 @@ trait ContentFunctions
     }
 
     /**
-     *
+     * This method travels recursively upwards in the document tree until the
+     * given depth from $level is reached an returns a reference to the reached
+     * object
      * @param int $level
-     * @return Document|DocumentContentElement
+     * @return DocumentContentStructure
      */
     public function toParentAtLevel($level)
     {
@@ -253,6 +309,7 @@ trait ContentFunctions
             return $this->getParent()->toParentAtLevel($level);
         }
     }
+
     /**
      *
      * @return int
@@ -270,7 +327,6 @@ trait ContentFunctions
      *
      * @return string
      */
-
     public function getNumberingStyle()
     {
         return $this->numberingStyle;
